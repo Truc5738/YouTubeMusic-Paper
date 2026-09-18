8e78431d3f4dfc869b87a22e3934f5fdc5c79cea
Truc5738 <hyung0101978@gmail.com>
2026-09-18 13:41:20 +0700

    feat: pass persistent storage into Bedrock UI

--- src/main/java/vn/youtubemusic/geyser/BedrockUiBridge.java
+++ src/main/java/vn/youtubemusic/geyser/BedrockUiBridge.java
@@ -2,17 +2,18 @@ package vn.youtubemusic.geyser;
 
 import org.bukkit.entity.Player;
 import vn.youtubemusic.music.MusicManager;
+import vn.youtubemusic.storage.Storage;
 
 /** Optional Bedrock UI bridge. Contains no Floodgate/Geyser classes so Java-only servers remain safe. */
 public interface BedrockUiBridge {
     boolean isBedrock(Player player);
     void open(Player player);
 
-    static BedrockUiBridge create(MusicManager music, boolean floodgatePresent) {
+    static BedrockUiBridge create(MusicManager music, Storage storage, boolean floodgatePresent) {
         if (!floodgatePresent) return new Noop();
         try {
             Class<?> type = Class.forName("vn.youtubemusic.geyser.BedrockMusicUi");
-            return (BedrockUiBridge) type.getConstructor(MusicManager.class).newInstance(music);
+            return (BedrockUiBridge) type.getConstructor(MusicManager.class, Storage.class).newInstance(music, storage);
         } catch (Throwable ignored) {
             return new Noop();
         }
