b33f0e65b4d17722cc13cc4ba7851633966cccbf
Truc5738 <hyung0101978@gmail.com>
2026-09-18 13:42:49 +0700

    release: update startup version to 2.10.0

--- src/main/java/vn/youtubemusic/YouTubeMusicPlugin.java
+++ src/main/java/vn/youtubemusic/YouTubeMusicPlugin.java
@@ -16,7 +16,7 @@ public final class YouTubeMusicPlugin extends JavaPlugin {
         saveResource("messages_vi.yml", false);
         int javaFeature = Runtime.version().feature();
         if (javaFeature < 25) {
-            getLogger().severe("YouTubeMusic 2.9.0 yêu cầu Java 25 trở lên. Java hiện tại: " + Runtime.version());
+            getLogger().severe("YouTubeMusic 2.10.0 yêu cầu Java 25 trở lên. Java hiện tại: " + Runtime.version());
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
